a241351257fd74aa2cec89885c2eb25242d78202
jack <543612779@qq.com>
2019-04-19 09:13:23 +0800

    Add react-loadable

--- src/Router/index.js
+++ src/Router/index.js
@@ -4,27 +4,24 @@ import {
   Route,
   Switch
 } from "react-router-dom";
-// import Loadable from 'react-loadable';
-// import App from '../Containers/App/index.tsx';
-import App from '../Containers/First/index.js';
-// import Step3 from '../Containers/Step3/index.js';
+import Loadable from 'react-loadable';
+import App from '../Containers/App/index.tsx';
+import Step3 from '../Containers/Step3/index.js';
 
-// const Loading = () => <div>loading</div>
-//
-// const loadComponet = Loadable({
-//   loader: () => import('../Containers/Step2/index.js'),
-//   loading: Loading
-// })
+const Loading = () => <div>loading</div>
 
-// const RouterConfig = () =>
-//     <Router>
-//       <Switch>
-//         <Route exact path='/' component={App} />
-//         <Route exact path='/Step2' component={loadComponet} />
-//         <Route exact path='/Step3' component={Step3} />
-//       </Switch>
-//     </Router>
+const loadComponet = Loadable({
+  loader: () => import('../Containers/First/index.js'),
+  loading: Loading
+})
 
-const RouterConfig = () => <App/>
+const RouterConfig = () =>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route exact path='/Step2' component={loadComponet} />
+        <Route exact path='/Step3' component={Step3} />
+      </Switch>
+    </Router>
 
 export default RouterConfig;
