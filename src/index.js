78433588008642bec6ca4415243859f11bb053a0
jack <13919952895@163.com>
2019-04-19 09:06:48 +0800

    Add react-hot-loader

--- src/index.js
+++ src/index.js
@@ -25,20 +25,20 @@ const render = (Component) => {
 
 render(RouterConfig);
 
-// if (module.hot) {
-//   /**
-// 	* Warning from React Router, caused by react-hot-loader.
-// 	* The warning can be safely ignored, so filter it from the console.
-// 	* Otherwise you'll see it every time something changes.
-// 	* See https://github.com/gaearon/react-hot-loader/issues/298
-// 	*/
-//   const orgError = console.error;// eslint-disable-line no-console
-//   // eslint-disable-next-line no-console
-//   console.error = (message) => {
-//     if (message && message.indexOf('You cannot change <Router routes>;') === -1) {
-//       // Log the error as normally
-//       orgError.apply(console, [message]);
-//     }
-//   };
-//   module.hot.accept('./Containers/App/index.js', () => { render(App); });
-// }
+if (module.hot) {
+  /**
+	* Warning from React Router, caused by react-hot-loader.
+	* The warning can be safely ignored, so filter it from the console.
+	* Otherwise you'll see it every time something changes.
+	* See https://github.com/gaearon/react-hot-loader/issues/298
+	*/
+  const orgError = console.error;// eslint-disable-line no-console
+  // eslint-disable-next-line no-console
+  console.error = (message) => {
+    if (message && message.indexOf('You cannot change <Router routes>;') === -1) {
+      // Log the error as normally
+      orgError.apply(console, [message]);
+    }
+  };
+  module.hot.accept('./Router/index.js', () => { render(RouterConfig); });
+}
